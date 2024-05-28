0c2b3af2d04ccc95b030a53bc8eabe38fc457da8
Tharshini <jeytharshi@gmail.com>
2024-05-28 07:59:20 -0400

    cleaned the runner class

--- src/test/java/runners/ScoreRunnerTest.java
+++ src/test/java/runners/ScoreRunnerTest.java
@@ -4,8 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		
-		
+				
 		features = { "src/test/resources/Features"},
 		glue = {"StepDefinition","BaseStep"},
 		tags = "@tag6",
